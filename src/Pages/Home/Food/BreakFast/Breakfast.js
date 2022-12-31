70f6527d1434e029f9fc85c9e4d60b9d92b5c844
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-12-31 13:17:04 +0600

    initial setup

---
+++ src/Pages/Home/Food/BreakFast/Breakfast.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Breakfast = () => {
+    return (
+        <div>
+            <h2>this is breakfast.</h2>
+        </div>
+    );
+};
+
+export default Breakfast;
