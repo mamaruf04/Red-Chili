36cd9f6a688976e5c68fa87dcdeed7628f127736
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-01-04 03:06:19 +0600

    relaive food added

--- src/Pages/Home/Food/FoodDetails/FoodDetails.js
+++ src/Pages/Home/Food/FoodDetails/FoodDetails.js
@@ -1,13 +1,21 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  MinusIcon,
+  PlusIcon,
+  ShoppingCartIcon
+} from "@heroicons/react/24/outline";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import useFood from "../../../../hooks/useFood";
+import "./FoodDetails.css";
 
 const FoodDetails = () => {
+  const [count, setCount] = useState(1);
+  const [relative, setRelative] = useState([]);
   const { categories } = useFood();
   const { foodCategory } = useParams();
   const { foodId } = useParams();
   const categoryParams = foodCategory || "breakfast";
-
+  const navigate = useNavigate();
   //   const [foods, setFoods] = useState([]);
   //   useEffect(() => {
   //     fetch("food.json")
@@ -15,6 +23,12 @@ const FoodDetails = () => {
   //       .then((data) => setFoods(data?.menu));
   //   }, []);
 
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
 
   // find the matched category.
   const selectedCategory = categories?.find(
@@ -26,13 +40,66 @@ const FoodDetails = () => {
     (singleFood) => singleFood.id === parseFloat(foodId)
   );
 
+  // useEffect(()=>{
+  //   selectedCategory?.items?.map(relate => setRelative(relate))
+  // },[selectedCategory])
+
+  // console.log(relative);
+
   return (
     <div>
       <div className="flex justify-center gap-10 w-11/12 max-w-5xl mx-auto">
-        <div className="flex flex-col gap-6 p-10 max-w-xl">
+        <div className="flex flex-col gap-6 px-10 max-w-xl">
           <h1 className="text-6xl font-semibold">{selectedFood?.name}</h1>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, rem. Distinctio aliquam voluptatibus hic nisi explicabo at adipisci impedit quibusdam?</p>
-          <h2 className="text-4xl font-semibold">${selectedFood?.price}</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam,
+            rem. Distinctio aliquam voluptatibus hic nisi explicabo at adipisci
+            impedit quibusdam?
+          </p>
+          <div className="flex  gap-5">
+            <h2 className="text-4xl font-semibold">
+              ${(selectedFood?.price * count).toFixed(2)}
+            </h2>
+            <div className="flex items-center border rounded-full">
+              <button
+                className="px-3 py-2 font-bold text-lg"
+                onClick={handleDecrease}
+              >
+                <MinusIcon
+                  className={
+                    count > 1
+                      ? "h-6 w-6 text-[#F91944] hover:text-[#e10d37]"
+                      : "h-6 w-6"
+                  }
+                />
+              </button>
+              <span className="px-3 py-2 text-lg font-semibold">{count}</span>
+              <button
+                className="px-3 py-2 font-bold text-lg"
+                onClick={handleIncrease}
+              >
+                <PlusIcon className="h-6 w-6 text-[#F91944] hover:text-[#e10d37]" />
+              </button>
+            </div>
+          </div>
+          <button className="w-28 mb-10 py-2 border rounded-full bg-[#F91944] hover:bg-[#e10d37] text-white">
+            <div className="flex justify-center items-center gap-2">
+              <ShoppingCartIcon className="h-6 w-6" />
+              <span>Add</span>
+            </div>
+          </button>
+
+          {/* <p className="text-gray-600">Relative foods <hr /></p> */}
+          <div className=" flex justify-start items-center overflow-scroll overflow-y-hidden gap-2">
+            {selectedCategory?.items?.map((relate) => (
+              <div key={relate?.id} onClick={() => navigate(`${relate.id}`)}>
+                <img className="w-36" src={relate?.image} alt="" />
+                <p className="w-36 font-semibold text-center">
+                  <small>{relate?.name}</small>
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
         <div>
           <img width={"450px"} src={selectedFood?.image} alt="" />
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -8,13 +8,13 @@ const Header = () => {
         <img src={logo} alt="Logo" className="h-8" />
       </div>
       <div className="flex items-center">
-        <button className="px-4 py-2 rounded-lg  text-black hover:text-red-600 focus:outline-none focus:shadow-outline">
+        <button className="px-4 py-2 rounded-lg  text-black hover:text-[#F91944] focus:outline-none focus:shadow-outline">
           Cart
         </button>
-        <button className="px-4 py-1 rounded-full bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:shadow-outline ml-4">
+        <button className="px-4 py-1 rounded-full bg-[#F91944] text-white hover:bg-[#e10d37] focus:outline-none focus:shadow-outline ml-4">
           Login
         </button>
-        <button className="px-4 py-1 rounded-full bg-red-500 text-white hover:bg-red-600 focus:outline-none focus:shadow-outline ml-4">
+        <button className="px-4 py-1 rounded-full bg-[#F91944] text-white hover:bg-[#e10d37] focus:outline-none focus:shadow-outline ml-4">
           Sign up
         </button>
       </div>
